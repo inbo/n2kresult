8d9cf6d7a85ffd769c33074374fd300ecb2cece2
ThierryO <Thierry.Onkelinx@inbo.be>
2016-10-29 13:51:27 +0200

    update unit test settings

--- ut_update.sh
+++ ut_update.sh
@@ -1,1 +1,1 @@
-~/liquibase/liquibase --url=jdbc:postgresql://localhost:5432/n2kunittest --changeLogFile=update.yml update
+~/liquibase/liquibase --username=ut_owner --password=+ZPNpj8Pgv_AbrNZ --url=jdbc:postgresql://localhost:5432/n2kunittest --changeLogFile=update.yml update
